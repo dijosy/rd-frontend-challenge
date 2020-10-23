ea4f3c2ba4296cc34d475c8ccc49cb7ba2091d36
Maura PC <josydimaura@gmail.com>
2020-10-23 19:20:18 -0300

    conclusão teste

--- assets/js/bundle.js
+++ assets/js/bundle.js
@@ -67,7 +67,7 @@
         users.map(i=>{
             let li = create('li')
             li.classList.add('lista-usuarios')
-            li.innerHTML = ` <img src="${i.avatar_url}"/> <span>  ${i.login}</span> `;
+            li.innerHTML = ` <a href="${i.url}" ><img src="${i.avatar_url}"/> <span>  ${i.login}</span></a> `;
             Ul.appendChild(li)
         })
         app.appendChild(Ul)
